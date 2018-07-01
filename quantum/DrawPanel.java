cd9cddf444286dd4deab9f7518a08a5f7aad019f
Digory <digory1@googlemail.com>
2018-07-01 11:13:58 +0100

    Replace Synthesizer with Sequencer to enable notes to be displayed one by one

--- quantum/DrawPanel.java
+++ quantum/DrawPanel.java
@@ -5,48 +5,54 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.util.List;
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.ShortMessage;
 import javax.swing.JPanel;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements ControllerEventListener{
 
     private final List<Integer> decimals;
+    private int location = -1;
 
     public DrawPanel(List<Integer> decimals) {
         this.decimals = decimals;
     }
 
-    private String getMusicNotes() {
+    private String getMusicNote() {
         /*
         This method is for figuring out what the note name (you know: "C#" or
         whatever) of the MIDI decimal value is; so that we can display this to 
         the user.
          */
-        StringBuilder musicNotes = new StringBuilder();
         String[] notes = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-        for (int i = 0; i < decimals.size(); i++) {
-            int note = decimals.get(i);
-            note %= 12;
-            if (i < decimals.size() - 1) {
-                musicNotes.append(notes[note] + ", ");
-            } else {
-                musicNotes.append(notes[note]);
-            }
-        }
-        return musicNotes.toString();
+        return notes[decimals.get(location)%12];
     }
 
     @Override
     public void paintComponent(Graphics g) {
+        if (location == -1){
+            return; 
+            /*
+            ^ So that it doesn't paint the first note before the play button is
+            pushed.
+            */
+        }
         g.setColor(Color.CYAN);
-        g.fillOval(0, 135, 830, 30);
+        g.fillOval((30*location), 135, 30, 30);
         g.setFont(new Font("default", Font.BOLD, 15));
         g.setColor(Color.BLACK);
-        g.drawString(getMusicNotes(), 65, 155);
+        g.drawString(getMusicNote(), (30*location+10), 155);
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(830, 300);
+        return new Dimension(960, 300);
+    }
+
+    @Override
+    public void controlChange(ShortMessage event) {
+        location = event.getData2();
+        repaint();
     }
 
 }
--- quantum/UserInterface.java
+++ quantum/UserInterface.java
@@ -6,20 +6,24 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.sound.midi.Instrument;
-import javax.sound.midi.MidiChannel;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class UserInterface implements Runnable{
-private JFrame frame;
-private final List<Integer> decimals;
+public class UserInterface implements Runnable {
 
-    public UserInterface(List<Integer> decimals){
+    private JFrame frame;
+    private final List<Integer> decimals;
+
+    public UserInterface(List<Integer> decimals) {
         this.decimals = decimals;
     }
 
@@ -28,44 +32,51 @@ private final List<Integer> decimals;
         JFrame frame = new JFrame("Quantum Music");
         frame.setBackground(Color.BLACK);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         createComponents(frame.getContentPane());
         frame.pack();
-  
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
-        DrawPanel panel = new DrawPanel(decimals);   
+        DrawPanel panel = new DrawPanel(decimals);
+        Sequencer sequencer = createSequencer(panel);
         JButton startButton = new JButton(new AbstractAction("Play") {
             @Override
             public void actionPerformed(ActionEvent e) {
-                createSynthesizer();
+                sequencer.start();
             }
         });
         panel.add(startButton);
         container.add(panel);
     }
-    
-    private void createSynthesizer(){
-        Synthesizer midiSynth;
-    try {
-        midiSynth = MidiSystem.getSynthesizer();
-        midiSynth.open();
-        Instrument[] instr = midiSynth.getDefaultSoundbank().getInstruments();
-        MidiChannel[] mChannels = midiSynth.getChannels();
-        midiSynth.loadInstrument(instr[0]);
 
-        for (Integer elements : decimals) {
-            mChannels[0].noteOn(60 + elements, 100);
-            try {           
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(UserInterface.class.getName()).log(Level.SEVERE, null, ex);
+    private Sequencer createSequencer(DrawPanel panel) {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            Sequence sequence;
+            sequence = new Sequence(Sequence.PPQ, 3);
+            Track track = sequence.createTrack();           
+            /* 
+            We go through the decimal notes and add them to the track. The 
+            panel is told the location (i.e. where we are in the list of notes)
+            with use of the ControllerEventListener interface, so that it can 
+            display the current note to the user.
+            */
+            int location = 0;            
+            for (int elements : decimals) {
+                track.add(new MidiEvent(new ShortMessage(ShortMessage.CONTROL_CHANGE, 0, 0, location), (3 * location)));
+                track.add(new MidiEvent(new ShortMessage(ShortMessage.NOTE_ON, 0, elements + 36, elements + 36), (3 * location)));
+                track.add(new MidiEvent(new ShortMessage(ShortMessage.NOTE_OFF, 0, elements + 36, elements + 36), (3 * location) + 3));
+                location++;
             }
+            sequencer.open();
+            sequencer.setSequence(sequence);
+            sequencer.addControllerEventListener(panel, new int[]{0});
+            return sequencer;
+        } catch (InvalidMidiDataException | MidiUnavailableException ex) {
+            Logger.getLogger(UserInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
-    } catch (MidiUnavailableException ex) {
-        Logger.getLogger(UserInterface.class.getName()).log(Level.SEVERE, null, ex);
+        return null;
     }
-    }
-
 }
